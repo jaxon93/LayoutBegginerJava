76711ac218c98fcf0f35341cce63cbff5bde7fda
jaxon93 <jaxon932002@gmail.com>
2023-05-18 20:56:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/layoutbegginerjava/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutbegginerjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
